55207391f9f76497157b76429ee65ba9000833fd
alumno <alumno@DESKTOP-IBBC2QF>
2021-10-27 10:20:57 +0200

    Agregar archivos de proyecto.

---
+++ AragonSkills/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AragonSkills.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
